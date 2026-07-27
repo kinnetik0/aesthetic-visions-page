f2f3740f26d6564d6dc137969b77001df4470439
Alexis Rosquete <alexis.rosquete@gmail.com>
2026-07-27 09:03:58 -0400

    Update App.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -13,7 +13,7 @@ const App = () => (
     <TooltipProvider>
       <Toaster />
       <Sonner />
-      <HashRouter>
+      <HashRouter basename="/aesthetic-visions-page/">
         <Routes>
           <Route path="/" element={<Index />} />
           {/* ADD ALL CUSTOM ROUTES ABOVE THE CATCH-ALL "*" ROUTE */}
